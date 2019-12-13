ca9b73c5960ee639c455dd10f4ed252e575240ec
Georg Jung <gjung@gimas.net>
2019-12-13 18:50:25 +0200

    Add working demo

---
+++ LinuxArpLookupDemo/LinuxArp.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LinuxArpLookupDemo
+{
+    public static class LinuxArp
+    {
+        private const string ArpTablePath = "/proc/net/arp";
+        private static readonly Regex lineRegex = new Regex(@"^((?:[0-9]{1,3}\.){3}[0-9]{1,3})(?:\s+\w+){2}\s+((?:[0-9A-Fa-f]{2}[:-]){5}(?:[0-9A-Fa-f]{2}))");
+
+        public static bool IsWindows() => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        private static bool IsPlatformSupported()
+        {
+            if (IsWindows())
+                return false;
+            return File.Exists(ArpTablePath);
+        }
+
+        public static async Task<PhysicalAddress> PingThenReadArpTable(IPAddress ip, TimeSpan timeout)
+        {
+            if (!IsPlatformSupported())
+                throw new PlatformNotSupportedException();
+            using var ping = new Ping();
+            var reply = await ping.SendPingAsync(ip, (int)timeout.TotalMilliseconds).ConfigureAwait(false);
+            return await TryReadFromArpTable(ip).ConfigureAwait(false);
+        }
+
+        public static async Task<PhysicalAddress> TryReadFromArpTable(IPAddress ip)
+        {
+            if (!IsPlatformSupported())
+                throw new PlatformNotSupportedException();
+            using var arpFile = new FileStream(ArpTablePath, FileMode.Open, FileAccess.Read);
+            return await ParseProcNetArp(arpFile, ip).ConfigureAwait(false);
+        }
+
+        private static async Task<PhysicalAddress> ParseProcNetArp(Stream content, IPAddress ip)
+        {
+            using var reader = new StreamReader(content);
+            await reader.ReadLineAsync().ConfigureAwait(false); // first line is header
+            while (!reader.EndOfStream)
+            {
+                var line = await reader.ReadLineAsync().ConfigureAwait(false);
+                if (string.IsNullOrWhiteSpace(line))
+                    return null;
+                try
+                {
+                    var mac = ParseIfMatch(line, ip);
+                    if (mac != null)
+                        return mac;
+                }
+                catch (FormatException)
+                {
+                    throw new PlatformNotSupportedException(); ;
+                }
+            }
+            return null;
+        }
+
+        private static PhysicalAddress ParseIfMatch(string line, IPAddress ip)
+        {
+            var m = lineRegex.Match(line);
+            if (!m.Success || m.Groups.Count != 3)
+                throw new FormatException($"The given line '{line}' was not in the expected /proc/net/arp format.");
+            var tableIpStr = m.Groups[1].Value;
+            var tableMacStr = m.Groups[2].Value;
+            var tableIp = IPAddress.Parse(tableIpStr);
+            if (!tableIp.Equals(ip))
+                return null;
+            return ParseMacAddress(tableMacStr);
+        }
+
+        private static PhysicalAddress ParseMacAddress(this string mac)
+        {
+            var macString = mac?.Replace(":", "-", StringComparison.Ordinal)?.ToUpper() ?? throw new ArgumentNullException(nameof(mac));
+            return PhysicalAddress.Parse(macString);
+        }
+    }
+}
---
+++ LinuxArpLookupDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace LinuxArpLookupDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args?.Length != 1)
+            {
+                Console.WriteLine("This program expects excalty one parameter, which is an IP address.");
+                return;
+            }
+
+            var ipStr = args[0];
+            if (!IPAddress.TryParse(ipStr, out var ip))
+            {
+                Console.WriteLine("The given parameter could not be parsed as IP address.");
+                return;
+            }
+
+            Console.WriteLine($"Trying to find mac of {ip}...");
+            var mac = await LinuxArp.PingThenReadArpTable(ip, TimeSpan.FromMilliseconds(1500)).ConfigureAwait(false);
+            if (mac == null)
+                Console.WriteLine("mac not found");
+            else
+                Console.WriteLine($"mac is {GetMacString(mac.GetAddressBytes())}");
+        }
+
+        public static string GetMacString(byte[] value)
+        {
+            return string.Join(":", from z in value select z.ToString("X2"));
+        }
+    }
+}
